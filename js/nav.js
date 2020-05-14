01a48963ef0e92bbfd0a676994abdcabfeab5a9f
Thomas Schallert <thomas.schallert@fhnw.ch>
2020-05-14 15:22:34 +0200

    extend burger navigation

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,23 @@
-// Selection of HTML objects
-const burger = document.querySelector('.burger i');
-const nav = document.querySelector('.nav');
+(function () {
+  const burger = document.querySelector('.burger i');
+  const nav = document.querySelector('.nav');
 
-// Defining a function
-const toggleNav = () => {
-  burger.classList.toggle('fa-times');
-  burger.classList.toggle('fa-bars');
+  const toggleNav = () => {
+    burger.classList.toggle('fa-times');
+    burger.classList.toggle('fa-bars');
+    nav.classList.toggle('nav-active');
+  };
 
-  nav.classList.toggle('nav-active');
-};
+  burger.addEventListener('click', function () {
+    toggleNav();
+  });
 
-// Calling the function after click event occurs
-burger.addEventListener('click', function () {
-  toggleNav();
-});
+  nav.addEventListener('click', function () {
+    toggleNav();
+  });
+
+  window.onhashchange = (e) => {
+    e.stopPropagation();
+    burger.classList.contains('fa-times') ? toggleNav() : '';
+  };
+})();
